1db8ada143a019a9b32e3f6cf3dc5d969a7233bb
polluque <polluque26081997@gmail.com>
2022-12-25 13:45:49 -0500

    firetask_paulluque_v1.2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,49 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  //const HomePage({super.key});
+  // con este objeto puedo apuntar a la coleccion en firebase 'tasks'
+  CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
+
 
   @override
   
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('FIREBASE FIRESTORE'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                tasksReference.get().then((QuerySnapshot value) {
+                  /*
+                  QuerySnapshot collection = value;
+                  List<QueryDocumentSnapshot> docs = collection.docs;
+                  QueryDocumentSnapshot doc = docs[0];
+                  // se imprime el id y la data por separado 
+                  print(doc.id);
+                  print(doc.data());
+                  */
+                  QuerySnapshot collection = value;
+                  collection.docs.forEach((QueryDocumentSnapshot element) { 
+                    Map<String, dynamic> myMap = element.data() as Map<String, dynamic>;
+                    print(myMap["title"]);
+                    //print(element.data());
+                  });
+
+                });
+              }, 
+              child: Text('obtener la data')
+              )
+          ],
+        ),
+      ),
       
     );
   }
